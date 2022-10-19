e3f329bf866ba179fc4f737f8996863827f866fb
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2022-10-20 02:26:53 +0600

    bug fix

--- src/controllers/conversation.js
+++ src/controllers/conversation.js
@@ -6,12 +6,20 @@ exports.createNewConversation = catchAssyncErrors(async (req, res, next) => {
     const newConversation = new Conversation({
         members: [req.body.senderId, req.body.receiverId],
     });
+    const conversation = await Conversation.find({
+        $and: [
+            { members: { $in: [req.body.senderId] } },
+            { members: { $in: [req.body.receiverId] } },
+        ],
+    });
+
+    if (conversation.length >= 1)
+        return next(new ErrorHandeler("Already have a conversation", 409));
 
     const isSave = await newConversation.save();
 
     if (!isSave)
         return next(new ErrorHandeler("Can't Create Conversition", 404));
-
     res.status(200).json({
         message: `Conversation created successfully with ${req.body.receiverName}`,
     });
