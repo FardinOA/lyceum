35de58a187b9ce0cff8a1becfa763e368e9d5214
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2022-12-14 00:05:34 +0600

    test deploy

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-const app = require("./app");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary");
 // Handling uncaught Error exceptions
+
+const express = require("express");
+const errorMiddleware = require("./middlewares/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config();
+app.use(cookieParser());
+app.use(
+    bodyParser.urlencoded({
+        limit: "50mb",
+        extended: false,
+    })
+);
+app.use(bodyParser.json({ limit: "50mb" }));
+
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
     console.log(`Shutting down the server due to uncaught Error exceptions`);
 
     process.exit(1);
 });
-
+app.use(
+    cors({
+        origin: [
+            "https://lyceum-frontend-git-main-fardinoa.vercel.app",
+            "https://lyceum-frontend.vercel.app",
+            "https://lyceum-frontend-fardinoa.vercel.app",
+            "http://localhost:3000",
+        ],
+        credentials: true,
+    })
+);
 const url = process.env.MONGO_URL;
 
 mongoose
@@ -35,6 +62,19 @@ cloudinary.config({
     api_secret: process.env.CLOUDINAY_API_SECRET,
 });
 
+//all routes
+
+const userRouter = require("./src/routes/userRoutes");
+
+const conversationRouter = require("./src/routes/conversation");
+const messageRouter = require("./src/routes/message");
+
+app.use("/api/v1", userRouter, conversationRouter, messageRouter);
+app.get("/", (req, res) => {
+    res.json({ message: "hello Fardin" });
+});
+//middleware for error
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
@@ -47,3 +87,4 @@ process.on("unhandledRejection", (err) => {
         process.exit(1);
     });
 });
+app.use(errorMiddleware);
