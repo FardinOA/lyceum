24977fd4540a5a2bb13632eb9f52b28ea5dc25f1
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2022-12-14 00:17:16 +0600

    test deploy

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const sendEmail = require("../../utils/sendEmail");
+// const sendEmail = require("../../utils/sendEmail");
 const userModel = require("../models/userModel");
 const User = require("../models/userModel");
 const Post = require("../models/postModel");
@@ -12,6 +12,28 @@ const catchAssyncErrors = require("../../middlewares/catchAssyncErrors");
 const Notification = require("../models/notificationModel");
 
 const cloudinary = require("cloudinary").v2;
+const nodeMailer = require("nodemailer");
+const sendEmail = async (options) => {
+    const transporter = nodeMailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port: 465,
+        secure: true,
+        service: process.env.SMPT_SERVICE,
+        auth: {
+            user: process.env.SMPT_MAIL,
+            pass: process.env.SMPT_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.SMPT_MAIL,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
 
 exports.registerUser = catchAssyncErrors(async (req, res, next) => {
     const otp = Math.floor(1000 + Math.random() * 9000);
