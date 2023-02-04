2d6a0bbabb94c10b43e46824152b1aedf219594d
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2023-02-04 08:37:51 +0600

    bug fix

--- utils/sendLoginToken.js
+++ utils/sendLoginToken.js
@@ -7,7 +7,7 @@ const sendToken = async (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        // httpOnly: true,
         sameSite: "None",
         secure: true,
     };
